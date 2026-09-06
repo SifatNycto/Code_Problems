a9a531485380cf131373c37cf9c6f5d3f57b97f1
SifatNycto <sftmhd@gmail.com>
2026-09-06 12:56:06 +0600

    Added some problems

---
+++ Codeforces_Problems/2259C_101.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int t;
+    std::cin >> t;
+
+    int n[t];
+    int answer[t];
+
+
+    while (t--)
+    {
+
+    }
+
+
+    return 0;
+}
+
+// 1 -> Nhoj
+// 0 -> John
