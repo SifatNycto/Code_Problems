04280933678ce39bc34e3083b5ccc57d5f2cbdc6
SifatNycto <sftmhd@gmail.com>
2026-09-19 02:25:35 +0600

    Add solution of Codeforces problem 467A George and Accommodation

--- Codeforces/467A_George_and_Accommodation.cpp
+++ Codeforces/467A_George_and_Accommodation.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-#include <string>
 
 int main()
 {
-    int n;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n, p, q, freeRooms = 0;
     std::cin >> n;
-    
+
+    while (n--)
+    {
+        std::cin >> p >> q;
+        if (q - p >= 2)
+            freeRooms++;
+    }
+
+    std::cout << freeRooms;
+
+    return 0;
 }
